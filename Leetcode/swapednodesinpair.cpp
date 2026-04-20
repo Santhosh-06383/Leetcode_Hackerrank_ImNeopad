4fd36962d3b2e03c9085d7b722e0b47282de7138
Santhosh kumar S <jyothisathis84@gmail.com>
2026-04-20 11:19:08 +0530

    Create swapednodesinpair.cpp

---
+++ Leetcode/swapednodesinpair.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        // If list is empty or has only one node
+        if (!head || !head->next) return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+
+        while (prev->next && prev->next->next) {
+            ListNode* first = prev->next;
+            ListNode* second = prev->next->next;
+
+            // Start the swap
+            first->next = second->next;
+            second->next = first;
+            prev->next = second;
+
+            // Move prev forward for the next pair
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+};
